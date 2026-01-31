a471fd91cb833632dbbddfd0d9ed850023ce87ad
Yassine Ben kacem <yassinbenkacem12@gmail.com>
2026-01-31 15:20:14 +0100

    mcp and agent connected

--- server/src/main/java/com/example/server/services/AIAgent.java
+++ server/src/main/java/com/example/server/services/AIAgent.java
@@ -1,28 +1,34 @@
 package com.example.server.services;
 
+import java.util.Arrays;
+
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.stereotype.Service;
-import com.example.server.tools.AITools;
 import lombok.Data;
 @Service
 @Data
 public class AIAgent {
 
-    private AITools aiTools;
     private ChatClient chatClient;
     
     public AIAgent(
         ChatClient.Builder builder,
-        ChatMemory memory
-        , AITools aiTools){
+        ChatMemory memory,
+        ToolCallbackProvider tools
+    ){
+        Arrays.stream(tools.getToolCallbacks()).forEach(tool->{
+            System.out.println("-------------------------------------------------------");
+            System.out.println(tool.getToolDefinition());
+
+        });
         this.chatClient = builder
             .defaultAdvisors(
                 MessageChatMemoryAdvisor.builder(memory)
                 .build()
-            )
-            .defaultTools(aiTools)
+            ).defaultToolCallbacks(tools)
             .build();
         
     }
